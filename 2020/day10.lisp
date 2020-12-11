1fb93f4b8280bd3b581e3e67c6a8c6bcfa65ff66
Teemu Liikala <tliikala@gmail.com>
2020-12-11 19:40:34 +0200

    Adds 2020 day 10 part 1.

--- 2020/day10.lisp
+++ 2020/day10.lisp
@@ -17,75 +17,10 @@
 (defvar *adapters* nil)
 
 (defun read-input-data ()
-  (let ((lines (read-lines-from-file "C:\\Users\\tliik\\Documents\\Ohjelmointi\\advent-of-code\\2020\\input-day10-test1.txt")))
+  (let ((lines (read-lines-from-file "C:\\Users\\tliik\\Documents\\Ohjelmointi\\advent-of-code\\2020\\input-day10.txt")))
     (setq *adapters* (loop for x in lines collect (make-instance 'adapter :joltage (parse-integer x))))))
 
-(defun ok-next-adapters (adapters)
-
-  ;(if (cdr adapters)
-
-  ;(print adapters)
-
-      (loop with here = (car adapters)
-            for x on (cdr adapters)
-            as diff = (- (joltage (car x)) (joltage here))
-            when (< 0 diff 4)
-            collect x
-            until (> diff 3))
-
-   ; (and adapters
-    ;     (let ((diff (- (joltage (car adapters)) latest-joltage)))
-     ;      (when (< 0 diff 4)
-      ;       (list (car adapters)))))
-
-    )
-
-
-(defun go-through (adapters gone-through)
-      
-
-
-
-  (let ((ok-adapters (ok-next-adapters adapters)))
-    
-
-
-    
-    (if ok-adapters (loop for x in ok-adapters
-                          as tulos = (go-through x (cons (car adapters) gone-through))
-                          when (when tulos ;(break (format nil "~A" gone-through))
-                               ;  (print gone-through)
-                                 t
-                                    ;(+ 1 2)
-                                    )
-                          append tulos)
-      (values (and (= (length gone-through) (length *adapters*)) (progn (break "ff") gone-through))))))
-      
-
-
-
-  
-;  (if (= (length gone-through) (length *adapters*)) gone-through
- ;   (let ((adapters (ok-next-adapters joltage gone-through)))
-  ;    (loop for a in adapters
-   ;         append (go-through (joltage a) (cons a gone-through))))))
-
-(defun day-10-part-1 ()
-  
-  ;(setq *adapters* (cons (make-instance 'adapter :joltage 0) (sort (read-input-data) #'< :key #'joltage)))
-
-  (setq *adapters* (sort (read-input-data) #'< :key #'joltage))
-
-
-  (let ((built-in-adapter (make-instance 'adapter :joltage (+ 3 (loop for x in *adapters* maximizing (joltage x))))))
-    
-    ;(setf (cdr (last *adapters*)) (list built-in-adapter))
-  
-  (let* (
-         
-         (result (go-through *adapters* nil))
-         (joltages (and result (cons (joltage built-in-adapter) (mapcar #'joltage result)))))
-    (count-differences-and-multiply joltages))))
+;;; Part 1
 
 (defun count-differences-and-multiply (joltages)
   (loop for x on joltages
@@ -93,6 +28,30 @@
         count (and (cadr x) (= (- (car x) (cadr x)) 1)) into diff1
         finally (return (* diff1 diff3))))
 
-;;; (day-10-part-1)
+(defun day-10-part-1 ()
+  (setq *adapters* (cons (make-instance 'adapter :joltage 0) (sort (read-input-data) #'< :key #'joltage)))
+  (let* ((result (go-through *adapters* nil))
+         (joltages (and result (cons (+ 3 (joltage (car result))) (mapcar #'joltage result)))))
+    (count-differences-and-multiply joltages)))
 
-;;; 2400 wrong too low
+(defmethod ok-next-adapters ((self adapter) rest-adapters)
+  (loop for x on rest-adapters
+        as diff = (- (joltage (car x)) (joltage self))
+        when (< 0 diff 4)
+        collect x
+        until (> diff 3)))
+
+(defun go-through2 (choices curr gone-through)
+  (or (go-through (car choices) (cons curr gone-through))
+      (go-through2 (cdr choices) curr gone-through)))
+
+(defun go-through (adapters gone-through)
+  (if adapters
+      (lw:when-let (choices (if (cdr adapters) (ok-next-adapters (car adapters) (cdr adapters))
+                              ;;; Would the final adapter be ok?
+                              (let ((c (car adapters)) (p (car gone-through)))
+                                (and (< 0 (- (joltage c) (joltage p)) 4) (list (list c))))))
+        (go-through2 choices (car adapters) gone-through))
+    gone-through))
+
+;;; (day-10-part-1)
