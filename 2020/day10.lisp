0ded8d1558847685a42581aad0046523c444be6a
Teemu Liikala <tliikala@gmail.com>
2020-12-11 19:14:46 +0200

    2020 day 10 WIP.

--- 2020/day10.lisp
+++ 2020/day10.lisp
@@ -17,29 +17,75 @@
 (defvar *adapters* nil)
 
 (defun read-input-data ()
-  (let ((lines (read-lines-from-file "C:\\Users\\tliik\\Documents\\Ohjelmointi\\advent-of-code\\2020\\input-day10.txt")))
+  (let ((lines (read-lines-from-file "C:\\Users\\tliik\\Documents\\Ohjelmointi\\advent-of-code\\2020\\input-day10-test1.txt")))
     (setq *adapters* (loop for x in lines collect (make-instance 'adapter :joltage (parse-integer x))))))
 
-(defun ok-next-adapters (joltage &optional (not-these nil))
-  (loop for x in *adapters*
-        as diff = (- (joltage x) joltage)
-        when (and (< 0 diff 4) (not (member x not-these)))
-        collect x))
+(defun ok-next-adapters (adapters)
 
-;;; (go-through 0 nil)
+  ;(if (cdr adapters)
 
-(defun go-through (joltage gone-through)
-  (if (= (length gone-through) (length *adapters*)) gone-through
-    (let ((adapters (ok-next-adapters joltage gone-through)))
-      (loop for a in adapters
-            append (go-through (joltage a) (cons a gone-through))))))
+  ;(print adapters)
+
+      (loop with here = (car adapters)
+            for x on (cdr adapters)
+            as diff = (- (joltage (car x)) (joltage here))
+            when (< 0 diff 4)
+            collect x
+            until (> diff 3))
+
+   ; (and adapters
+    ;     (let ((diff (- (joltage (car adapters)) latest-joltage)))
+     ;      (when (< 0 diff 4)
+      ;       (list (car adapters)))))
+
+    )
+
+
+(defun go-through (adapters gone-through)
+      
+
+
+
+  (let ((ok-adapters (ok-next-adapters adapters)))
+    
+
+
+    
+    (if ok-adapters (loop for x in ok-adapters
+                          as tulos = (go-through x (cons (car adapters) gone-through))
+                          when (when tulos ;(break (format nil "~A" gone-through))
+                               ;  (print gone-through)
+                                 t
+                                    ;(+ 1 2)
+                                    )
+                          append tulos)
+      (values (and (= (length gone-through) (length *adapters*)) (progn (break "ff") gone-through))))))
+      
+
+
+
+  
+;  (if (= (length gone-through) (length *adapters*)) gone-through
+ ;   (let ((adapters (ok-next-adapters joltage gone-through)))
+  ;    (loop for a in adapters
+   ;         append (go-through (joltage a) (cons a gone-through))))))
 
 (defun day-10-part-1 ()
-  (read-input-data)
-  (let* ((result (go-through 0 nil))
-         (joltages (cons (+ (joltage (car result)) 3) (mapcar #'joltage result))))
-    (setf (cdr (last joltages)) (list 0))
-    (count-differences-and-multiply joltages)))
+  
+  ;(setq *adapters* (cons (make-instance 'adapter :joltage 0) (sort (read-input-data) #'< :key #'joltage)))
+
+  (setq *adapters* (sort (read-input-data) #'< :key #'joltage))
+
+
+  (let ((built-in-adapter (make-instance 'adapter :joltage (+ 3 (loop for x in *adapters* maximizing (joltage x))))))
+    
+    ;(setf (cdr (last *adapters*)) (list built-in-adapter))
+  
+  (let* (
+         
+         (result (go-through *adapters* nil))
+         (joltages (and result (cons (joltage built-in-adapter) (mapcar #'joltage result)))))
+    (count-differences-and-multiply joltages))))
 
 (defun count-differences-and-multiply (joltages)
   (loop for x on joltages
@@ -47,4 +93,6 @@
         count (and (cadr x) (= (- (car x) (cadr x)) 1)) into diff1
         finally (return (* diff1 diff3))))
 
-;;;(day-10-part-1)
+;;; (day-10-part-1)
+
+;;; 2400 wrong too low
